3f8484edf5143b2409236bfcd5183e553f96520c
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-09-01 13:00:43 +0530

    stream and listen for identity updates

--- src/sse.py
+++ src/sse.py
@@ -1,10 +1,13 @@
 import asyncio
 from datetime import datetime
 from functools import lru_cache
+from hashlib import sha1
 
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import Request
+from sqlalchemy import delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.sql import text
@@ -13,6 +16,7 @@ from sse_starlette.sse import EventSourceResponse
 from .settings import Settings
 from .sse_models import Base
 from .sse_models import Environment
+from .sse_models import Identity
 
 engine = create_async_engine("sqlite+aiosqlite:///:memory:", future=True)
 
@@ -47,6 +51,19 @@ async def queue_environment_changes(environment_key: str):
     return
 
 
+@router.post("/sse/environments/{environment_key}/identities/{identifier}/queue-change")
+async def queue_identity_changes(environment_key: str, identifier: str):
+    async with AsyncSession(engine, autoflush=True) as session:
+        statement = text(
+            """INSERT OR REPLACE INTO identity(identifier, environment_key) VALUES(:identifier, :environment_key)"""
+        )
+        await session.execute(
+            statement, {"identifier": identifier, "environment_key": environment_key}
+        )
+        await session.commit()
+    return
+
+
 @router.get("/sse/environments/{environment_key}/stream")
 async def stream_environment_changes(
     request: Request, environment_key: str, settings: Settings = Depends(get_settings)
@@ -61,8 +78,28 @@ async def stream_environment_changes(
                 environment_updated = True
                 await session.delete(environment)
 
+                # Clear identity updates if the environment was updated
+                await session.execute(
+                    delete(Identity).where(Identity.environment_key == environment_key)
+                )
             await session.commit()
             return environment_updated
+
+        async def get_updated_identities():
+            identities = await session.execute(
+                select(Identity.identifier).where(
+                    Identity.environment_key == environment_key
+                )
+            )
+            hashed_identities = [
+                sha1(identity[0].encode()).hexdigest() for identity in identities
+            ]
+            await session.execute(
+                delete(Identity).where(Identity.environment_key == environment_key)
+            )
+            await session.commit()
+
+            return hashed_identities
 
         async def event_generator():
             while True:
@@ -80,6 +117,12 @@ async def stream_environment_changes(
                         "event": "environment_updated",
                         "retry": settings.retry_timeout,
                     }
+                elif hashed_identities := await get_updated_identities():
+                    for identity in hashed_identities:
+                        yield {
+                            "event": "identity_updated",
+                            "data": {"identifier_hash": identity},
+                        }
 
                 await asyncio.sleep(settings.stream_delay)
 

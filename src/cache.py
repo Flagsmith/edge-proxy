45b290427cda16a65892b24440363f01ec8c6ce5
Gonçalo Silva <goncalossilva@gmail.com>
2023-09-22 01:02:53 +0100

    Don't use logging directly

--- src/cache.py
+++ src/cache.py
@@ -5,6 +5,8 @@ import requests
 
 from .settings import Settings
 
+logger = logging.getLogger(__name__)
+
 
 class CacheService:
     def __init__(self, settings: Settings):
@@ -30,7 +32,7 @@ class CacheService:
                 )
             except requests.exceptions.HTTPError:
                 received_error = True
-                logging.error(
+                logger.error(
                     f"received non 200 response for {key_pair.client_side_key}"
                 )
         if not received_error:

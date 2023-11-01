69e01a5d4ab549483f2220e59a458ec3a00ed2ee
Gonçalo Silva <goncalossilva@gmail.com>
2023-11-01 02:33:53 +0000

    Modernize codebase to be fully async (#77)
    * Modernize codebase to be fully async
    
    * Make API poll timeout configurable
    
    * Default to 5 seconds on API polling requests
    
    5 seconds is also httpx's default.

--- src/cache.py
+++ src/cache.py
@@ -1,8 +1,8 @@
 import logging
 from datetime import datetime
 
+import httpx
 import orjson
-import requests
 
 from .exceptions import FlagsmithUnknownKeyError
 from .settings import Settings
@@ -14,25 +14,25 @@ class CacheService:
     def __init__(self, settings: Settings):
         self.settings = settings
         self.last_updated_at = None
-        self._session = requests.Session()
         self._cache = {}
+        self._client = httpx.AsyncClient(timeout=settings.api_poll_timeout)
 
-    def fetch_document(self, server_side_key):
-        url = f"{self.settings.api_url}/environment-document/"
-        response = self._session.get(
-            url, headers={"X-Environment-Key": server_side_key}
+    async def fetch_document(self, server_side_key):
+        response = await self._client.get(
+            url=f"{self.settings.api_url}/environment-document/",
+            headers={"X-Environment-Key": server_side_key},
         )
         response.raise_for_status()
         return orjson.loads(response.text)
 
-    def refresh(self):
+    async def refresh(self):
         received_error = False
         for key_pair in self.settings.environment_key_pairs:
             try:
-                self._cache[key_pair.client_side_key] = self.fetch_document(
+                self._cache[key_pair.client_side_key] = await self.fetch_document(
                     key_pair.server_side_key
                 )
-            except (requests.exceptions.HTTPError, orjson.JSONDecodeError):
+            except (httpx.HTTPError, orjson.JSONDecodeError):
                 received_error = True
                 logger.exception(
                     f"Failed to fetch document for {key_pair.client_side_key}"

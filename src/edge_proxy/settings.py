2aa89724f74a1c6fd9e801217cbdd667320c4499
Rodrigo López Dato <rodrigo.lopezdato@flagsmith.com>
2025-01-22 13:29:01 -0300

    Silence warning when rendering default config

--- src/edge_proxy/settings.py
+++ src/edge_proxy/settings.py
@@ -113,7 +113,7 @@ class AppSettings(BaseModel):
             )
         ]
     )
-    api_url: HttpUrl = "https://edge.api.flagsmith.com/api/v1"
+    api_url: HttpUrl = HttpUrl("https://edge.api.flagsmith.com/api/v1")
     api_poll_frequency_seconds: int = Field(
         default=10,
         validation_alias=AliasChoices(
